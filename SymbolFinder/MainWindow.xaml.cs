c979305c694517f9c15a9e27e8be4102d87088cd
Andreas Aakvik Gogstad <agogstad@gmail.com>
2025-03-18 16:30:49 +0100

    Add project files.

---
+++ SymbolFinder/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Diagnostics.SymbolStore;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SymbolFinder;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+
+    List<UnicodeSymbol> Symbols = [];
+    public ObservableCollection<UnicodeSymbol> SearchResults = [];
+
+    /*
+        Code_Point;
+        Name;
+        General_Category;
+        Canonical_Combining_Class;
+        Bidi_Class;
+        Decomposition_Type_and_Decomposition_Mapping;
+        Numeric_Type;
+        Numeric_Value_for_Type_Digit;
+        Numeric_Value_for_Type_Numeric;
+        Bidi_Mirrored;
+        Unicode_1_Name;
+        ISO_Comment;
+        Simple_Uppercase_Mapping;
+        Simple_Lowercase_Mapping;
+        Simple_Titlecase_Mapping
+    */
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        LoadUnicodeFile(@"data\UnicodeData.txt");
+        this.DataContext = this;
+        ResultBox.ItemsSource = SearchResults;
+    }
+
+    private void ClickSearch(object sender, RoutedEventArgs e)
+    {
+        SearchSymbols();
+    }
+
+    private void SearchSymbols()
+    {
+        string searchTerm = TextboxSearch.Text;
+
+        SearchResults.Clear();
+
+        foreach (UnicodeSymbol symbol in Symbols)
+        {
+            StringBuilder sb = new();
+            if (symbol.Contains(searchTerm))
+            {
+                SearchResults.Add(symbol);
+            }
+        }
+        Debug.WriteLine($"Found {SearchResults.Count} among {Symbols.Count} matching {searchTerm}");
+    }
+
+    private void LoadUnicodeFile(string filename)
+    {
+        if (File.Exists(filename) == false)
+        {
+            Debug.WriteLine($"No such unicode text file: {filename}");
+        }
+
+        string[] unicodeLines = File.ReadAllLines(filename);
+
+        DateTime before = DateTime.Now;
+
+        int counter = 0; // test safeguard, implement timer if read takes too long
+        foreach (string line in unicodeLines)
+        {
+            string[] values = line.Split(';');
+            if (values.Length < 5)
+            {
+                Debug.WriteLine($"Too few values on line {counter}");
+                continue;
+            }
+            
+            Symbols.Add(new UnicodeSymbol(values));
+            counter++;
+        }
+
+        TimeSpan processingTime = DateTime.Now - before;
+        Debug.WriteLine($"Read all line in {(int)processingTime.TotalSeconds}s {processingTime.Milliseconds}");
+    }
+
+    private void ResultBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        UnicodeSymbol? symbol = (UnicodeSymbol)ResultBox.SelectedItem;
+        if (symbol != null)
+        {
+            Debug.WriteLine($"Copy {symbol.Name}");
+        }
+        else
+        {
+            Debug.WriteLine($"Selected symbol was null");
+        }
+        CopySymbolToClipboard(symbol);
+    }
+
+    private void CopySymbolToClipboard(UnicodeSymbol? symbol)
+    {
+        if (symbol == null) return;
+        Clipboard.SetText(symbol.Symbol.ToString());
+    }
+
+    private void TextboxSearch_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            SearchSymbols();
+        }
+    }
+}
---
+++ SymbolFinder/UnicodeSymbol.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymbolFinder
+{
+    public class UnicodeSymbol
+    {
+        public string Name { get; set; } // the display name of the character
+        public char Symbol { get; set; } // the actual unicode symbol character
+        public string CodePoint { get; set; } // unicode hex code
+
+        //public UnicodeSymbol(string name, string symbol, int number)
+        //{
+        //    throw new NotImplementedException();
+        //    Name = name;
+        //    Symbol = symbol;
+        //    CodePoint = number;
+        //}
+
+        public UnicodeSymbol(string[] values)
+        {
+            if (values.Length < (int)Importindex.END)
+            {
+                throw new IndexOutOfRangeException("Unicode values array is shorter than expected");
+            }
+            Name = values[(int)Importindex.Name];
+            CodePoint = values[(int)Importindex.Code_Point];
+            long codeNumber = Convert.ToInt64(CodePoint, 16);
+            Symbol = (char)codeNumber;
+            //Debug.WriteLine($"Name: {Name}   Codepoint: {CodePoint}   Symbol: {Symbol}.");
+        }
+
+        private UnicodeSymbol()
+        {
+            throw new NotImplementedException();
+        }
+
+        public enum Importindex
+        {
+            Code_Point = 0,
+            Name = 1,
+            General_Category = 2,
+            Canonical_Combining_Class = 3,
+            Bidi_Class = 4,
+            Decomposition_Type_and_Decomposition_Mapping = 5,
+            Numeric_Type = 6,
+            Numeric_Value_for_Type_Digit = 7,
+            Numeric_Value_for_Type_Numeric = 8,
+            Bidi_Mirrored = 9,
+            Unicode_1_Name = 10,
+            ISO_Comment = 11,
+            Simple_Uppercase_Mapping = 12,
+            Simple_Lowercase_Mapping = 13,
+            Simple_Titlecase_Mapping = 14,
+            END
+        }
+
+        public bool Contains(string searchTerm)
+        {
+            return (Name.Contains(searchTerm, StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
